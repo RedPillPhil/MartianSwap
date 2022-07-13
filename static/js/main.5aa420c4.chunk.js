bf2a597147635d59230aec631d6256e8e1e34a5e
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-07-13 06:37:25 -0400

    Update main.5aa420c4.chunk.js

--- static/js/main.5aa420c4.chunk.js
+++ static/js/main.5aa420c4.chunk.js
@@ -318,7 +318,7 @@
         X = t(27),
         J = t(13),
         Z = t(5),
-        $ = "0x86ad49a1d665087c6cce9d5874b8bc2a23e0f8a0",
+        $ = "0x36048Dd829B97A9fAe73e372F5646175A406687d",
         ee = new Z.j(Z.a.MAINNET, "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", 18, "DAI", "Dai Stablecoin"),
         ne = new Z.j(Z.a.MAINNET, "0xe9e7cea3dedca5984780bafc599bd69add087d56", 18, "BUSD", "Binance USD"),
         te = new Z.j(Z.a.MAINNET, "0x55d398326f99059ff775485246999027b3197955", 18, "USDT", "Tether USD"),
