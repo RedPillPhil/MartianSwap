add72282b79a37b464e4dd535e53d00f81d8b956
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-07-13 01:44:50 -0400

    Update main.5aa420c4.chunk.js

--- static/js/main.5aa420c4.chunk.js
+++ static/js/main.5aa420c4.chunk.js
@@ -4652,7 +4652,7 @@
           console.info('Failed to parse input amount: "'.concat(e, '"'), r)
         }
       }
-      var Is = ["0xBCfCcbde45cE874adCB698cC183deBcF17952812", "0xf164fC0Ec4E93095b804a4795bBe1e041497b92a", "0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F"];
+      var Is = ["0xBCfCcbde45cE874adCB698cC183deBcF17952812", "0x86ad49A1d665087C6Cce9D5874b8Bc2a23e0F8A0", "0x86ad49A1d665087C6Cce9D5874b8Bc2a23e0F8A0"];
 
       function Es(e, n) {
         return e.route.path.some((function(e) {
