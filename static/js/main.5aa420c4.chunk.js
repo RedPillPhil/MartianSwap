a35d3a72efb7fbfac91e2ef51c29af9ff56f5b40
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-07-13 15:51:12 -0400

    Update main.5aa420c4.chunk.js

--- static/js/main.5aa420c4.chunk.js
+++ static/js/main.5aa420c4.chunk.js
@@ -318,7 +318,7 @@
         X = t(27),
         J = t(13),
         Z = t(5),
-        $ = "0x5Ed5f77543D29b7876627F0aA1fa96Ea40B3eFfc",
+        $ = "0x139D70B635344BaeeA2f29541f76e64f0764A20C",
         ee = new Z.j(Z.a.MAINNET, "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", 18, "DAI", "Dai Stablecoin"),
         ne = new Z.j(Z.a.MAINNET, "0xe9e7cea3dedca5984780bafc599bd69add087d56", 18, "BUSD", "Binance USD"),
         te = new Z.j(Z.a.MAINNET, "0x55d398326f99059ff775485246999027b3197955", 18, "USDT", "Tether USD"),
@@ -4652,7 +4652,7 @@
           console.info('Failed to parse input amount: "'.concat(e, '"'), r)
         }
       }
-      var Is = [" 0x01bF7C66c6BD861915CdaaE475042d3c4BaE16A7", "0x5Ed5f77543D29b7876627F0aA1fa96Ea40B3eFfc", "0x5Ed5f77543D29b7876627F0aA1fa96Ea40B3eFfc"];
+      var Is = ["0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73", "0xf164fC0Ec4E93095b804a4795bBe1e041497b92a", "0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F"];
 
       function Es(e, n) {
         return e.route.path.some((function(e) {
